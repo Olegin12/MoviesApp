78a3f99bbfce372c5bfa7f9fe70fea7a1e6f4813
Olegin12 <olegrodionov12@gmail.com>
2023-01-09 22:35:49 +0300

    NEW: garland animation

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import {next, prev} from "./Buttons/navigationBut.js";
 import {selectedTop} from "./Buttons/selectedTop.js";
 import {searchFilms} from "./API/searchFilms.js";
+import {garland} from "./style/garland.js"
 
+garland();
 selectedTop();
 searchFilms();
 
